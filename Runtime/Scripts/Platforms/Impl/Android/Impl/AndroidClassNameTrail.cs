5ea851821f2fb2fe1db79649ce20a1ea727b511f
p.shestakov <0xShestakov@gmail.com>
2024-01-19 22:39:05 +0300

    added additional trails (about class names and libraries)

---
+++ Runtime/Scripts/Platforms/Impl/Android/Impl/AndroidClassNameTrail.cs
@@ -0,0 +1,21 @@
+﻿#if UNITY_ANDROID
+using UnityEngine;
+
+namespace BloodseekerSDK.Android
+{
+    public sealed class AndroidClassNameTrail : IAndroidTrail
+    {
+        private string[] _classNames;
+
+        public AndroidClassNameTrail(string[] classNames)
+        {
+            this._classNames = classNames;
+        }
+
+        public AndroidJavaObject AsJavaObject()
+        {
+            return new AndroidJavaObject("com.am1goo.bloodseeker.trails.ClassNameTrail", _classNames);
+        }
+    }
+}
+#endif
---
+++ Runtime/Scripts/Platforms/Impl/Android/Impl/AndroidLibraryTrail.cs
@@ -0,0 +1,21 @@
+﻿#if UNITY_ANDROID
+using UnityEngine;
+
+namespace BloodseekerSDK.Android
+{
+    public sealed class AndroidLibraryTrail : IAndroidTrail
+    {
+        private string[] _libraryNames;
+
+        public AndroidLibraryTrail(string[] libraryNames)
+        {
+            this._libraryNames = libraryNames;
+        }
+
+        public AndroidJavaObject AsJavaObject()
+        {
+            return new AndroidJavaObject("com.am1goo.bloodseeker.trails.LibraryTrail", _libraryNames);
+        }
+    }
+}
+#endif
