0a8e0cda25e0be4dd52b2086255e512bd4d314eb
p.shestakov <0xShestakov@gmail.com>
2024-01-21 10:39:43 +0300

    added support of 'PathInApk' trails

---
+++ Runtime/Scripts/Platforms/Impl/Android/Impl/AndroidPathInApkTrail.cs
@@ -0,0 +1,21 @@
+﻿#if UNITY_ANDROID
+using UnityEngine;
+
+namespace BloodseekerSDK.Android
+{
+    public class AndroidPathInApkTrail : IAndroidTrail
+    {
+        private string[] _pathsInApk;
+
+        public AndroidPathInApkTrail(params string[] pathsInApk)
+        {
+            this._pathsInApk = pathsInApk;
+        }
+
+        public AndroidJavaObject AsJavaObject()
+        {
+            return new AndroidJavaObject("com.am1goo.bloodseeker.android.trails.PathInApkTrail", _pathsInApk);
+        }
+    }
+}
+#endif
