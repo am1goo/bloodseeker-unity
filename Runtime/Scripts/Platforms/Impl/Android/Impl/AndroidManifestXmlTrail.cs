0a2dd6bffe0459df1f7fcfcaecf9a6a16ef5b78a
p.shestakov <0xShestakov@gmail.com>
2024-01-21 21:56:20 +0300

    added brand new AndroidManifestXmlTrail (to works with AndroidManifest.xml in base apk)

---
+++ Runtime/Scripts/Platforms/Impl/Android/Impl/AndroidManifestXmlTrail.cs
@@ -0,0 +1,48 @@
+﻿#if UNITY_ANDROID
+using System;
+using System.Linq;
+using UnityEngine;
+
+namespace BloodseekerSDK.Android
+{
+    public sealed class AndroidManifestXmlTrail : IAndroidTrail
+    {
+        private Looker[] _lookers;
+
+        public AndroidManifestXmlTrail(params Looker[] lookers)
+        {
+            this._lookers = lookers;
+        }
+
+        public AndroidJavaObject AsJavaObject()
+        {
+            var strs = _lookers.Select(looker => JsonUtility.ToJson(looker)).ToArray();
+            return new AndroidJavaObject("com.am1goo.bloodseeker.android.trails.AndroidManifestXmlTrail", strs);
+        }
+
+        [Serializable]
+        public class Looker
+        {
+            public string[] nodes;
+            public string attribute;
+            public string value;
+
+            public Looker(string[] nodes, string attribute, string value)
+            {
+                this.nodes = nodes;
+                this.attribute = attribute;
+                this.value = value;
+            }
+
+            public static Looker UnityActivity()
+            {
+                return new Looker(
+                    nodes:      new[] { "application", "activity" },
+                    attribute:  "android:name",
+                    value:      "com.unity3d.player.UnityPlayerActivity"
+                );
+            }
+        }
+    }
+}
+#endif
