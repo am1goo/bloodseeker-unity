5a00c44724e654bfa350cd0a6a61a2bbb14dc0e7
p.shestakov <p.shestakov@gaijin.team>
2024-01-25 12:21:32 +0300

    added full support of generation *.bmx files via BmxGeneratorWindow

--- Editor/BmxGeneratorWindow.cs
+++ Editor/BmxGeneratorWindow.cs
@@ -1,21 +1,29 @@
 using System;
-using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using UnityEditor;
+#if UNITY_ANDROID
+using UnityEditor.Android;
+#endif
+using UnityEditor.PackageManager;
 using UnityEngine;
 using UnityEngine.UIElements;
 
 public class BmxGeneratorWindow : EditorWindow
 {
+    private const string PACKAGE_NAME = "com.am1goo.bloodseeker";
+
     private const string PP_PATH_TO_PROJECT_FILE = "pp_path_to_project_file";
     private const string PP_PATH_TO_RESULT_DIR = "pp_path_to_result_dir";
     private const string PP_PATH_TO_RESULT_FILENAME = "pp_path_to_result_filename";
+    private const string PP_REVEAL_IN_FINDER_AFTER_GENERATION = "pp_reveal_in_finder_after_generation";
 
     private const string PROJECT_FILENAME = "project.json";
     private const string DEFAULT_FILENAME = "generated.bmx";
 
     private TextField _pathToProjectField;
     private TextField _pathToResultField;
+    private Toggle _revealInFinderCheckbox;
     private Button _generateButton;
     private Label _logs;
 
@@ -28,6 +36,10 @@ public class BmxGeneratorWindow : EditorWindow
 
     private void Update()
     {
+        var prevRevealInFinder = EditorPrefs.GetBool(PP_REVEAL_IN_FINDER_AFTER_GENERATION);
+        if (prevRevealInFinder != _revealInFinderCheckbox.value)
+            EditorPrefs.SetBool(PP_REVEAL_IN_FINDER_AFTER_GENERATION, _revealInFinderCheckbox.value);
+
         var generateDenied = string.IsNullOrEmpty(_pathToProjectField.value) || string.IsNullOrEmpty(_pathToResultField.value);
         var generateAllowed = !generateDenied;
         _generateButton.SetEnabled(generateAllowed);
@@ -58,6 +70,9 @@ public class BmxGeneratorWindow : EditorWindow
         _pathToResultField.Add(pathToResultButton);
         root.Add(_pathToResultField);
 
+        _revealInFinderCheckbox = new Toggle("Open file after generation");
+        root.Add(_revealInFinderCheckbox);
+
         _generateButton = new Button(OnGenerateClick);
         _generateButton.text = "Generate";
         root.Add(_generateButton);
@@ -72,6 +87,9 @@ public class BmxGeneratorWindow : EditorWindow
         var pathToResultFilename = EditorPrefs.GetString(PP_PATH_TO_RESULT_FILENAME, string.Empty);
         var pathToResultFile = Path.Combine(pathToResultDir, pathToResultFilename);
         OnPathToResultChanged(pathToResultFile);
+
+        var revealInFinder = EditorPrefs.GetBool(PP_REVEAL_IN_FINDER_AFTER_GENERATION);
+        OnRevealInFinderChanged(revealInFinder);
     }
 
     private void OnPathToProjectClick()
@@ -109,6 +127,11 @@ public class BmxGeneratorWindow : EditorWindow
         _pathToResultField.value = path;
     }
 
+    private void OnRevealInFinderChanged(bool value)
+    {
+        _revealInFinderCheckbox.value = value;
+    }
+
     private void OnGenerateClick()
     {
         ClearLog();
@@ -122,15 +145,22 @@ public class BmxGeneratorWindow : EditorWindow
 
         var pathToResultFileInfo = new FileInfo(_pathToResultField.value);
         AddLogInfo($"Prepare to save to {pathToResultFileInfo.FullName}");
-        //TODO: invoke generator here
 
-        int retCode = UnityEngine.Random.Range(0, 2);
-        if (retCode != 0)
+        var exec = Exec(pathToProjectFileInfo.DirectoryName, pathToResultFileInfo.FullName);
+        if (exec.retCode != 0)
         {
+            AddLogError(exec.output);
             AddLogError("Failed!");
             return;
         }
+
+        AddLogInfo(exec.output);
         AddLogInfo("Success!");
+
+        if (!_revealInFinderCheckbox.value)
+            return;
+
+        EditorUtility.RevealInFinder(pathToResultFileInfo.FullName);
     }
 
     private void ClearLog()
@@ -153,6 +183,115 @@ public class BmxGeneratorWindow : EditorWindow
         if (_logs.text.Length > 0)
             _logs.text += Environment.NewLine;
 
-        _logs.text = $"[{prefix}] {text}";
+        _logs.text += $"[{prefix}] {text}";
+    }
+
+    private static (int retCode, string output) Exec(string pathToProject, string pathToResult)
+    {
+        var toolsPath = GetToolPath();
+        if (string.IsNullOrWhiteSpace(toolsPath))
+            return (-1, "can't file tools path");
+
+        var javaPath = GetJavaPath();
+        var generatorPath = Path.Combine(toolsPath, "Editor/Tools/generator.jar");
+        return Exec(javaPath, "-jar", generatorPath, pathToProject, pathToResult);
+    }
+
+    private static (int retCode, string output) Exec(string fileName, params string[] args)
+    {
+        try
+        {
+            Process proc = new Process();
+            proc.StartInfo.FileName = fileName;
+            proc.StartInfo.Arguments = string.Join(" ", args);
+            proc.StartInfo.RedirectStandardOutput = true;
+            proc.StartInfo.RedirectStandardError = true;
+            proc.StartInfo.UseShellExecute = false;
+            proc.StartInfo.CreateNoWindow = true;
+            bool started = proc.Start();
+            if (!started)
+                return (-1, $"process {fileName} cannot be started");
+
+            proc.WaitForExit();
+            int retCode = proc.ExitCode;
+            string output = proc.StandardOutput.ReadToEnd();
+            string error = proc.StandardError.ReadToEnd();
+            return (retCode, string.Join(Environment.NewLine, output, error).Trim());
+        }
+        catch (Exception ex)
+        {
+            return (-1, ex.ToString());
+        }
+    }
+
+    private static string GetJavaPath()
+    {
+        var jdkPath = Environment.GetEnvironmentVariable("JAVA_HOME");
+#if BLOODSEEKER_USE_UNITY_JDK
+#if UNITY_ANDROID
+        if (string.IsNullOrWhiteSpace(jdkPath))
+            jdkPath = AndroidExternalToolsSettings.jdkRootPath;
+#endif
+#endif
+        if (string.IsNullOrWhiteSpace(jdkPath))
+            return "java";
+        else
+            return Path.Combine(jdkPath, "bin", "java.exe");
+    }
+
+    private static string GetToolPath()
+    {
+        if (TryGetToolsPathFromPackages(out var pathFromPackages))
+        {
+            return pathFromPackages;
+        }
+        else if (TryGetToolPathFromSources(out var pathFromSources))
+        {
+            return pathFromSources;
+        }
+        else
+        {
+            return "";
+        }
+    }
+
+    private static bool TryGetToolsPathFromPackages(out string result)
+    {
+        var req = Client.List();
+        while (!req.IsCompleted)
+            System.Threading.Thread.Sleep(10);
+
+        var packages = req.Result;
+        if (packages == null)
+        {
+            result = default;
+            return false;
+        }
+
+        foreach (var package in packages)
+        {
+            if (package.name == PACKAGE_NAME)
+            {
+                result = package.resolvedPath;
+                return true;
+            }
+        }
+
+        result = default;
+        return false;
+    }
+
+    private static bool TryGetToolPathFromSources(out string result)
+    {
+        var path = Path.GetFullPath("Assets/Package");
+        var di = new DirectoryInfo(path);
+        if (!di.Exists)
+        {
+            result = default;
+            return false;
+        }
+
+        result = path;
+        return true;
     }
 }
