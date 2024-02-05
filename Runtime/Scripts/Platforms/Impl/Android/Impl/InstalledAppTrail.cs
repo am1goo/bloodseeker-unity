9a3f4bb90bb46bf3658bc5e34d66474a03a81e2f
am1goo <0xShestakov@gmail.com>
2024-02-05 11:12:17 +0300

    added support of brand new InstalledAppTrail (using for finding installed apps on device)

---
+++ Runtime/Scripts/Platforms/Impl/Android/Impl/InstalledAppTrail.cs
@@ -0,0 +1,21 @@
+﻿#if UNITY_ANDROID
+using UnityEngine;
+
+namespace BloodseekerSDK.Android
+{
+    public class InstalledAppTrail : IAndroidTrail
+    {
+        private string[] _appNames;
+
+        public InstalledAppTrail(params string[] appNames)
+        {
+            this._appNames = appNames;
+        }
+
+        public AndroidJavaObject AsJavaObject()
+        {
+            return new AndroidJavaObject(new SecureString("^com.am1goo.bloodseeker.android.trails.InstalledAppTrail^"), _appNames);
+        }
+    }
+}
+#endif
